0e833fc7a99f1f3b9321768603c5de7799703cc2
kawser ahmmed <mkshuvo25350@gmail.com>
2025-07-27 01:40:36 +0600

    login page added

---
+++ src/main/java/com/example/smartmedicalinventory/controller/HelloController.java
@@ -0,0 +1,28 @@
+package com.example.smartmedicalinventory.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class HelloController {
+
+    @FXML
+    private Button loginbtn;
+
+    @FXML
+    private Button registerbtn;
+
+    @FXML
+    private TextField userid;
+
+    @FXML
+    private PasswordField userpswd;
+
+    @FXML
+    private Button usertype1;
+
+    @FXML
+    private Button usertype2;
+
+}
---
+++ src/main/java/com/example/smartmedicalinventory/model/Customer.java
@@ -0,0 +1,4 @@
+package com.example.smartmedicalinventory.model;
+
+public class Customer {
+}
